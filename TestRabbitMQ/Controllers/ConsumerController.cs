04a19f57af37314f80aa65e1f8d939388fd4d9d3
Thiago <thiag@NOTEBOOKTH>
2023-02-23 09:41:13 -0300

    Add project files.

---
+++ TestRabbitMQ/Controllers/ConsumerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestRabbitMQ.Controllers
+{
+    public class ConsumerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
